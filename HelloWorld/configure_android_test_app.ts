17c241be8a9b6d4efabeccfeae74b2430f9a92a6
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2021-01-25 12:27:58 -0800

    PR feedback

--- HelloWorld/configure_android_test_app.ts
+++ HelloWorld/configure_android_test_app.ts
@@ -12,10 +12,11 @@ exports.run = function () {
     "../apps/android/LwcTestApp/app/build/outputs/apk/debug/app-debug.apk"
   );
 
-  const acceptLicensesCommand =
-    process.platform === "win32"
-      ? "echo y| %ANDROID_HOME\\tools\\bin\\sdkmanager \"build-tools;29.0.2\""
-      : "yes | $ANDROID_HOME/tools/bin/sdkmanager \"build-tools;29.0.2\"";
+  const androidHome = process.platform === "win32" ? "%ANDROID_HOME%" : "$ANDROID_HOME";
+  const sdkmanager = path.join(androidHome, "tools", "bin", "sdkmanager");
+  const acceptLicensesCommand = process.platform === "win32"
+    ? `echo y| ${sdkmanager} \"build-tools;29.0.2\"`
+    : `yes | ${sdkmanager} \"build-tools;29.0.2\"`;
   childProcess.execSync(acceptLicensesCommand, { stdio: ["ignore", "pipe", "pipe"] });
 
   const compileCommand =
