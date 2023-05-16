31bd8f1d3f64a3c0be10f448d8a9f5bf078a87bf
Meisam Seyed Aliroteh <maliroteh@salesforce.com>
2023-05-16 09:29:39 -0700

    prettier format

--- NameAndSignatureCapture/force-app/main/default/lwc/signatureDemo/signatureDemo.js
+++ NameAndSignatureCapture/force-app/main/default/lwc/signatureDemo/signatureDemo.js
@@ -1,36 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
 export default class SignatureDemo extends LightningElement {
-    imgSrc;
+  imgSrc;
 
-    renderedCallback() {
-        document.fonts.forEach(font => { 
-            if (font.family === 'Great Vibes' && font.status === 'unloaded') {
-                // ensure the font is loaded so that signature pad could use it
-                font.load();
-            }
-        });
+  renderedCallback() {
+    document.fonts.forEach((font) => {
+      if (font.family === "Great Vibes" && font.status === "unloaded") {
+        // ensure the font is loaded so that signature pad could use it
+        font.load();
+      }
+    });
+  }
+
+  saveSignature() {
+    const pad = this.template.querySelector("c-signature-pad");
+    if (pad) {
+      const dataURL = pad.getSignature();
+      if (dataURL) {
+        // At this point you can consume the signature, for example by saving
+        // it to disk or uploading it to a Salesforce org/record.
+        // Here we just preview it in an image tag.
+        this.imgSrc = dataURL;
+      }
+    }
+  }
+
+  clearSignature() {
+    const pad = this.template.querySelector("c-signature-pad");
+    if (pad) {
+      pad.clearSignature();
     }
 
-    saveSignature() {
-        const pad = this.template.querySelector("c-signature-pad");
-        if (pad) {
-            const dataURL = pad.getSignature();
-            if (dataURL) {
-                // At this point you can consume the signature, for example by saving
-                // it to disk or uploading it to a Salesforce org/record.
-                // Here we just preview it in an image tag.
-                this.imgSrc = dataURL;
-            }
-        }
-    }
-
-    clearSignature() {
-        const pad = this.template.querySelector("c-signature-pad");
-        if (pad) {
-            pad.clearSignature();
-        }
-
-        this.imgSrc = null;
-    }
+    this.imgSrc = null;
+  }
 }
